b222778b14a0ad9c7a32d16b5d54b3a6e7fafa17
Rahul Nair <rnair12599@gmail.com>
2020-03-27 03:13:09 -0700

    Updates

---
+++ precache-manifest.ef919968d4e5a741444de9c260728cd9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94da042ae2807116f686531076af4fc7",
+    "url": "/boat-test-project/index.html"
+  },
+  {
+    "revision": "4fe5abd0b8f4aac79efa",
+    "url": "/boat-test-project/static/css/main.b36412b7.chunk.css"
+  },
+  {
+    "revision": "c2cc82f93ebafb89b804",
+    "url": "/boat-test-project/static/js/2.546fd0d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boat-test-project/static/js/2.546fd0d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe5abd0b8f4aac79efa",
+    "url": "/boat-test-project/static/js/main.c462dd03.chunk.js"
+  },
+  {
+    "revision": "15f16564a85b0e956d13",
+    "url": "/boat-test-project/static/js/runtime-main.605148b6.js"
+  },
+  {
+    "revision": "4f0023ebc349ef0633f5f1192571fabe",
+    "url": "/boat-test-project/static/media/DeaConnectionLogo.4f0023eb.png"
+  },
+  {
+    "revision": "38d3c95b932e213c664d696fa27c2edc",
+    "url": "/boat-test-project/static/media/IowanOldStBTRoman.38d3c95b.ttf"
+  },
+  {
+    "revision": "697384b3fae8c762225813977d2b1111",
+    "url": "/boat-test-project/static/media/andre-benz-MsMISAIe8Qw-unsplash.697384b3.png"
+  },
+  {
+    "revision": "029e66394fc856bbe38f051b17fd3d93",
+    "url": "/boat-test-project/static/media/artur-rutkowski-LDxq4MnYB5U-unsplash.029e6639.png"
+  },
+  {
+    "revision": "a1e80e7c886d2c8e90c3d433124fb2f2",
+    "url": "/boat-test-project/static/media/artur-rutkowski-LDxq4MnYB5U-unsplash.a1e80e7c.png"
+  },
+  {
+    "revision": "13191e194d8894893210b2f8914fa26c",
+    "url": "/boat-test-project/static/media/bantersnaps-Bxzrd0p6yOM-unsplash.13191e19.png"
+  },
+  {
+    "revision": "7c6546d475d1c4c6c68ee063e1dc81c7",
+    "url": "/boat-test-project/static/media/bantersnaps-Bxzrd0p6yOM-unsplash@3x.7c6546d4.png"
+  },
+  {
+    "revision": "0e272b9819d41a0ffcc6fc952c685b90",
+    "url": "/boat-test-project/static/media/hours_content.0e272b98.png"
+  },
+  {
+    "revision": "41b1db20b33ee1e91ffdda1e2dfa3ee5",
+    "url": "/boat-test-project/static/media/janek-holoubek-15gOocGd8Qc-unsplash@2x.41b1db20.png"
+  },
+  {
+    "revision": "17162c633ef1ee58f337501489477dd6",
+    "url": "/boat-test-project/static/media/mshtakan.17162c63.ttf"
+  },
+  {
+    "revision": "4b281e5cc2f006ffaefa80dc412cdc41",
+    "url": "/boat-test-project/static/media/new-york-extra-large_regular.4b281e5c.ttf"
+  }
+]);
